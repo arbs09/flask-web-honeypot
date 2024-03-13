267a59a71559ffb14760808af080a516f74c2f0e
@arbs09 <101005375+arbs09@users.noreply.github.com>
2024-03-13 19:37:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def check_path():
 @app.route('/<path:filename>')
 def report_rules(filename):
   ip = request.client_ip
-  if filename in ['xmlrpc.php', 'check.js', 'my1.php', '.env', 'admin.php', 'wlwmanifest.xml', '.DS_Store', '.htaccess', 'core.js', 'install.php', 'config.php', 'st.php', 'repeater.php', 'dropdown.php', 'cjfuns.php', 'file.php', '_all_dbs', 'config.json', 'login.action', 'sftp.json', 'style.php', 'setup-config', 'hetong.js' ]:
+  if filename in ['xmlrpc.php', 'check.js', 'my1.php', '.env', 'admin.php', 'wlwmanifest.xml', '.DS_Store', '.htaccess', 'core.js', 'install.php', 'config.php', 'st.php', 'repeater.php', 'dropdown.php', 'cjfuns.php', 'file.php', '_all_dbs', 'config.json', 'login.action', 'sftp.json', 'style.php', 'setup-config?step=1', 'hetong.js' ]:
     if ip not in reported_ips or datetime.now() - reported_ips[ip] > REPORT_INTERVAL:
       save_to_file(ip)
       report_ip(ip, '18,19,21,15', f'Automated report for accessing {filename} on my Honeypot')
